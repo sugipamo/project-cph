e9579e8de26f47e3cd7cae13117cacb309eccb63
sugipamo <sugipamo@gmail.com>
2025-05-19 08:05:24 +0000

    generate_run_requestsメソッドの引数を修正し、未実装のTODOコメントを追加しました。これにより、リクエスト生成の処理が明確になりました。

--- src/execution_env/env_workflow_service.py
+++ src/execution_env/env_workflow_service.py
@@ -1,7 +1,5 @@
 from typing import List
 from src.execution_env.env_resource_controller import EnvResourceController
-from src.execution_env.env_context_loader import load_env_context_from_language_env, load_env_json
-from src.execution_env.env_context_loader import EnvContext, OjContext
 import copy
 from src.operations.composite_request import CompositeRequest
 
@@ -9,8 +7,9 @@ class EnvWorkflowService:
     def __init__(self):
         pass
 
-    def generate_run_requests(self, ):
+    def generate_run_requests(self):
         # TODO
         # Controllerを呼び出し
         # jsonの設定をもとにrequestを生成
         # requestはsrc/operationsを参照
+        pass
